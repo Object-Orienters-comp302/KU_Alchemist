69508cd9180bd2c8d7d0e90d332f2bd8322d4acc
HasanGorkem <horta21@ku.edu.tr>
2023-11-29 17:21:55 +0300

    Triangluar table nearly done, will add some assets

--- src/GUI_Components/TrianglePanel.java
+++ src/GUI_Components/TrianglePanel.java
@@ -1,35 +1,64 @@
 package GUI_Components;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridBagLayout;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class TrianglePanel extends JPanel {
-		private Color triangleColor;//TODO: can be changed to hexcode later
+	private Color triangleColor;//TODO: can be changed to hexcode later
 
-	    // Constructor that takes a Color parameter
-	    public TrianglePanel(Color triangleColor) {
-	        this.triangleColor = triangleColor;
-	    }
+    
+    public TrianglePanel(Color triangleColor) {
+    	
+        this.triangleColor = triangleColor;
 
-	    @Override
-	    protected void paintComponent(Graphics g) {
-	        super.paintComponent(g);
+    }
+    
+    public TrianglePanel(String color) {
+    	
+        this.triangleColor = Color.decode(color);
 
-	        int width = getWidth();
-	        int height = getHeight();
+    }
 
-	        // Define the three points of the triangle
-	        int[] xPoints = {0, width / 2, width};
-	        int[] yPoints = {height, 0, height};
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
-	        // Set the color of the triangle
-	        g.setColor(triangleColor); // You can set your desired color here
+        int width = getWidth();
+        int height = getHeight();
 
-	        // Fill the triangle
-	        g.fillPolygon(xPoints, yPoints, 3);
-	    }
+        // Define the three points of the triangle
+        int[] xPoints = {0, width / 2, width};
+        int[] yPoints = {height, 0, height};
 
-	    
+        // Set the color of the triangle
+        g.setColor(triangleColor); // You can set your desired color here
+
+        // Fill the triangle
+        g.fillPolygon(xPoints, yPoints, 3);
+    }
+    
+   
+    
+    
+    
+    public static void main(String[] args) {
+		//TriangleTable tri = new TriangleTable("#34ebcf");
+
+
+		JFrame frame = new JFrame("test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1200, 900);
+        frame.getContentPane().setLayout(new GridBagLayout());
+        TrianglePanel login = new TrianglePanel(Color.red);
+        login.setPreferredSize(new Dimension(800, 400));
+        frame.getContentPane().add(login);
+        frame.setVisible(true);
 	}
+
+    
+}
