e28b99641ac477a02d6d1f77991e2a2971f5843f
Mehmet Murat Budak <mbudak21@ku.edu.tr>
2023-12-01 23:09:55 +0300

    comments for LoginView

--- src/UI/LoginView.java
+++ src/UI/LoginView.java
@@ -7,6 +7,8 @@ import java.awt.*;
 
 public class LoginView {
     public void displayLoginView() {
+        // This function creates a JFrame and adds the LoginPage JPanel to it.
+        // This function is called from the LoginController.
         new Token("khorne","./Images/tokens/khorne.png","./Images/backgrounds/khorne_background.png");
         new Token("nurgle","./Images/tokens/nurgle.png","./Images/backgrounds/nurgle_background.png");
         new Token("slaanesh","./Images/tokens/slaanesh.png","./Images/backgrounds/slaanesh_background.png");
