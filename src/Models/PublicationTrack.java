413f0e5990ce877862a24f30bb4a6a94be3d2eaa
Alimetesevincli <asevincli21@ku.edu.tr>
2023-12-05 18:41:23 +0300

    PublicationTrack updated

--- src/Models/PublicationTrack.java
+++ src/Models/PublicationTrack.java
@@ -30,6 +30,22 @@ public class PublicationTrack {
     public void removePublicationCard(PublicationCard publicationCard) {
         publicationCards.remove(publicationCard);
     }
-    
+    public boolean searchInPublicationTrack(Ingredient ingredient, ArrayList<Aspect> aspects) {
+        List<PublicationCard> publicationCards = getPublicationCards();
+        int flag_ingredient = 0;
+        int flag_aspects = 0;
+        for (PublicationCard card : publicationCards) {
+            if (card.getIngredient().equals(ingredient)) {
+                flag_ingredient = 1; // PublicationCard ingredient found in the track
+            }
+            if (card.getAspects().equals(aspects)) {
+                flag_aspects = 1;  // PublicationCard aspects found in the track
+            }
+        }
+        if (flag_aspects == 0 && flag_ingredient == 0){
+            return false;
+        }
+        return true; // PublicationCard found in the track
+    }
     
 }
