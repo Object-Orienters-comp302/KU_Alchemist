818a19989d3ed27f5e328c633a3090001d26651f
Eray Altun <ealtun21@ku.edu.tr>
2023-12-05 17:35:31 +0300

    Added doc for event

--- src/Domain/event/Type.java
+++ src/Domain/event/Type.java
@@ -1,5 +1,9 @@
 package Domain.event;
 
+/**
+ * Enum defining different types of events.
+ * These types are used to categorize events in the system.
+ */
 public enum Type {
     GOLD,
     ARTIFACT,
