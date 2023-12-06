4e89980a4a1ac1e86b82ebdecb78e32cda958da7
MeteYokus2402 <myokus21@ku.edu.tr>
2023-12-06 15:35:59 +0300

    Added a textField

--- src/GUI_Components/ForageGrounds.java
+++ src/GUI_Components/ForageGrounds.java
@@ -1,9 +1,10 @@
 package GUI_Components;
 
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import GUI_Components.ImagePanel;
 import Utils.AssetLoader;
@@ -15,6 +16,9 @@ public class ForageGrounds extends JPanel {
      */
     ImagePanel Background;
     ImagePanel Card;
+    JTextField textField;
+    
+    
     public ForageGrounds() {
         this.setSize(1000,500);
         setLayout(null);
@@ -26,6 +30,16 @@ public class ForageGrounds extends JPanel {
         Background = new ImagePanel(AssetLoader.getAssetPath(AssetLoader.ForageGroundsAssets.Card));
         Background.setBounds(0,0 ,1000, 500);
         this.add(Background);
+        
+        textField = new JTextField();
+        textField.setForeground(Color.BLACK);
+        textField.setText("To forage press the card!! It costs 1 gold.");
+        textField.setFont(new Font("Century Schoolbook", Font.BOLD | Font.ITALIC, 26));
+        textField.setBounds(228, 294, 554, 96);
+        Background.add(textField);
+        textField.setColumns(10);
+        
+        textField.setOpaque(false);
         
         Card.addMouseMotionListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
