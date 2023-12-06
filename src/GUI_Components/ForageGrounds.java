fc6b6125fab641938f5900d7c13c865011fc932a
MeteYokus2402 <myokus21@ku.edu.tr>
2023-12-06 14:56:31 +0300

    Used AssetLoader

--- src/GUI_Components/ForageGrounds.java
+++ src/GUI_Components/ForageGrounds.java
@@ -6,6 +6,7 @@ import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
 
 import GUI_Components.ImagePanel;
+import Utils.AssetLoader;
 
 public class ForageGrounds extends JPanel {
     
@@ -18,17 +19,17 @@ public class ForageGrounds extends JPanel {
         this.setSize(1000,500);
         setLayout(null);
         
-        Card = new ImagePanel("C:/Workspace/GameBoard/Images/backgrounds/CardSized.png"); //Dummy Path.
+        Card = new ImagePanel(AssetLoader.getAssetPath(AssetLoader.ForageGroundsAssets.Background));
         Card.setBounds(832,240 ,158, 250);
         this.add(Card);
         
-        Background = new ImagePanel("C:/Workspace/GameBoard/Images/backgrounds/forageGrounds.png");//Dummy Path Because I didn't use enum
+        Background = new ImagePanel(AssetLoader.getAssetPath(AssetLoader.ForageGroundsAssets.Card));
         Background.setBounds(0,0 ,1000, 500);
         this.add(Background);
         
         Card.addMouseMotionListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-                //Dummy Code
+                //TODO Dummy Code
             }
         });
         
