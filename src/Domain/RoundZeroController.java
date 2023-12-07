5817edd3aeb1e5c9484b876012deed6c2a7ecb2b
Eray Altun <ealtun21@ku.edu.tr>
2023-12-07 16:23:22 +0300

    Fixed RoundZeroController having playlist as data

--- src/Domain/RoundZeroController.java
+++ src/Domain/RoundZeroController.java
@@ -4,24 +4,21 @@ import Models.Deck;
 import Models.Ingredient;
 import Models.Player;
 
-import java.util.ArrayList;
-
 public class RoundZeroController {
     private final Deck deck;
-    ArrayList<Player> players;
-    public RoundZeroController() {
-        this.players = Player.getPlayers();
+    
+    public RoundZeroController () {
         this.deck = Deck.getInstance();
     }
     
-    public void gameSetup() {
+    public void gameSetup () {
         
         //Creates 4 of each ingredient card in deck
         initializeIngredients(4);
         
         this.deck.shuffleIngredients();
         
-        for(Player player : players) {
+        for (Player player : Player.getPlayers()) {
             
             //Distributes starting gold
             gold_setup(player, 10);
@@ -31,20 +28,22 @@ public class RoundZeroController {
         }
     }
     
-    public void gold_setup(Player player, int gold) {
-        player.getInventory().setGold(gold);
-    }
-    
-    public void initializeIngredients(int quantity) {
+    public void initializeIngredients (int quantity) {
         for (Ingredient.IngredientTypes type : Ingredient.IngredientTypes.values()) {
             deck.addIngredient(new Ingredient(type), quantity);
         }
     }
     
-    private void dealIngredientCards(Player player, int count) {
+    public void gold_setup (Player player, int gold) {
+        player.getInventory()
+                .setGold(gold);
+    }
+    
+    private void dealIngredientCards (Player player, int count) {
         for (int i = 0; i < count; i++) {
             Ingredient card = deck.popIngredient();
-            player.getInventory().addIngredient(card, 1);
+            player.getInventory()
+                    .addIngredient(card, 1);
         }
     }
     
