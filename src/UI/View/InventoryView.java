6e50d69eac02dd19bec543d4999d19c20a716d05
MeteYokus2402 <myokus21@ku.edu.tr>
2023-12-19 20:10:13 +0300

    Added next round, fixed a bug in inventory and a minor fix in help screen

--- src/UI/View/InventoryView.java
+++ src/UI/View/InventoryView.java
@@ -286,6 +286,16 @@ public class InventoryView extends JPanel implements Listener {
             }
         }
     }
+    private void IngredientsQuantityReset(){
+        feather_quantity.setText("0");
+        feet_quantity.setText("0");
+        flower_quantity.setText("0");
+        frog_quantity.setText("0");
+        mandrake_quantity.setText("0");
+        mushroom_quantity.setText("0");
+        scorpion_quantity.setText("0");
+        weed_quantity.setText("0");
+    }
     
     public void PotionsQuantity() {
         
@@ -317,6 +327,7 @@ public class InventoryView extends JPanel implements Listener {
     @Override
     public void onEvent(Type type) {
         if (type == Type.INGREDIENT) {
+            IngredientsQuantityReset();
             IngredientsQuantity();
             this.revalidate();
             this.repaint();
