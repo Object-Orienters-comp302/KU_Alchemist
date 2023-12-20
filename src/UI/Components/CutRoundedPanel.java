35c2d918663f9543432493c35a5e8dbe30d01bfd
Eray Altun <ealtun21@ku.edu.tr>
2023-12-20 23:10:00 +0300

    uncommented setClip

--- src/UI/Components/CutRoundedPanel.java
+++ src/UI/Components/CutRoundedPanel.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.geom.RoundRectangle2D;
 
 public class CutRoundedPanel extends JPanel {
     
@@ -31,7 +32,7 @@ public class CutRoundedPanel extends JPanel {
     protected void paintChildren(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        // g2.setClip(new RoundRectangle2D.Float(0, 0, getWidth(), getHeight(), cornerRadius, cornerRadius));
+        g2.setClip(new RoundRectangle2D.Float(0, 0, getWidth(), getHeight(), cornerRadius, cornerRadius));
         super.paintChildren(g2);
         g2.dispose();
     }
