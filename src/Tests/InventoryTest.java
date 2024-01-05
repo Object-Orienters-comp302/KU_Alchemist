2eed0b890bf7681816bc10df8a40e53a88e34686
Mehmet Murat Budak <91738477+mbudak21@users.noreply.github.com>
2024-01-05 23:35:35 +0300

    fix .iml file issue

--- src/Tests/InventoryTest.java
+++ src/Tests/InventoryTest.java
@@ -1,50 +1,50 @@
 package Tests;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import Models.Artifact;
+import org.junit.jupiter.api.*;
+
 import Models.Ingredient;
 import Models.Inventory;
-import org.junit.*;
 
 
 
 public class InventoryTest {
     private Inventory inventory;
     
-    @Before
+    @BeforeEach
     public void setUp() {
         inventory = new Inventory();
     }
     
     @Test
     public void testInitialization() {
-        assertNotNull("Ingredients map should be initialized", inventory.getIngredientHashMap());
-        assertNotNull("Potions map should be initialized", inventory.getPotions());
-        assertNotNull("Artifacts map should be initialized", inventory.getArtifacts());
-        assertEquals("Gold should be initialized to 0", (Integer) 0, inventory.getGold());
+        assertNotNull(inventory.getIngredientHashMap(), "Ingredients map should be initialized");
+        assertNotNull(inventory.getPotions(), "Potions map should be initialized");
+        assertNotNull(inventory.getArtifacts(),"Artifacts map should be initialized");
+        assertEquals((Integer) 0, inventory.getGold(),"Gold should be initialized to 0");
     }
     
     @Test
     public void testAddAndRemoveIngredient() {
         inventory.addIngredient(Ingredient.IngredientTypes.Feather, 5);
-        assertEquals("Ingredient count should be 5", 5, inventory.getIngredientCount(Ingredient.IngredientTypes.Feather));
+        assertEquals(5, inventory.getIngredientCount(Ingredient.IngredientTypes.Feather), "Ingredient count should be 5");
         
         inventory.removeIngredient(Ingredient.IngredientTypes.Feather);
-        assertFalse("Ingredient should be removed", inventory.isInInventory(Ingredient.IngredientTypes.Feather));
+        assertFalse(inventory.isInInventory(Ingredient.IngredientTypes.Feather), "Ingredient should be removed");
     }
     @Test
     public void testAddAndRemoveIngredient2() {
         inventory.addIngredient(Ingredient.IngredientTypes.Feather, 3);
-        assertEquals("Ingredient count should be 5", 3, inventory.getIngredientCount(Ingredient.IngredientTypes.Feather));
+        assertEquals(3, inventory.getIngredientCount(Ingredient.IngredientTypes.Feather), "Ingredient count should be 3");
         
         inventory.removeIngredient(Ingredient.IngredientTypes.Feather);
-        assertFalse("Ingredient should be removed", inventory.isInInventory(Ingredient.IngredientTypes.Feather));
+        assertFalse(inventory.isInInventory(Ingredient.IngredientTypes.Feather), "Ingredient should be removed");
     }
     @Test
     public void testAddGold() {
         inventory.addGold(100);
-        assertEquals("Gold count should be 100", (Integer)100, inventory.getGold());
+        assertEquals((Integer)100, inventory.getGold(),"Gold count should be 100");
     }
 //    @Test
 //    public void testArtifactExistenceAndRemoval() {
--- src/Tests/RoundOneControllerTest.java
+++ src/Tests/RoundOneControllerTest.java
@@ -19,45 +19,45 @@ import org.junit.jupiter.api.*;
 
 
 public class RoundOneControllerTest {
+    Player testPlayer;
+    Ingredient.IngredientTypes plant1 = Ingredient.IngredientTypes.Plant;
+    
+    @BeforeEach
+    public void init_classes(){
+        // Create a test player with some ingredients in the inventory
+        Player testPlayer = new Player("Test Player" , null);
+    }
     
 
     
     @Test
     public void testTransmuteIngredient_RemoveIngredientAndAddGold() {
         
-        // Create a test player with some ingredients in the inventory
-        Player testPlayer = new Player("Test Player" , null);
-        Ingredient plant1 = new Ingredient(Ingredient.IngredientTypes.Plant);
+        
         testPlayer.getInventory().addIngredient(plant1 , 1); //Add Plant to the inventory
         
-        GameController.getInstance().getRoundOneController().TransmuteIngredient(testPlayer, plant1.getType());
+        GameController.getInstance().getRoundOneController().TransmuteIngredient(testPlayer, plant1);
         
-        assertFalse(testPlayer.isInInventory(Ingredient.IngredientTypes.Plant));
+        assertFalse(testPlayer.getInventory().isInInventory(Ingredient.IngredientTypes.Plant));
         assertEquals(1, testPlayer.getInventory().getGold());
     }
     
     @Test
     public void testTransmuteIngredient_EmptyInventory_NoChanges() {
-        // Create a test player with some ingredients in the inventory
-        Player testPlayer = new Player("Test Player" , null);
-        Ingredient plant1 = new Ingredient(Ingredient.IngredientTypes.Plant);
-        GameController.getInstance().getRoundOneController().TransmuteIngredient(testPlayer, plant1.getType());
+        GameController.getInstance().getRoundOneController().TransmuteIngredient(testPlayer, plant1);
         
-        assertFalse(testPlayer.isInInventory(Ingredient.IngredientTypes.Plant));
+        assertFalse(testPlayer.getInventory().isInInventory(Ingredient.IngredientTypes.Plant));
         assertEquals(0, testPlayer.getInventory().getGold());
     }
     
     @Test
     public void testTransmuteIngredient_MultipleIngredientsOfType_RemoveOne() {
-        
-        Player testPlayer = new Player("Test Player" , null);
-        Ingredient plant1 = new Ingredient(Ingredient.IngredientTypes.Plant);
         testPlayer.getInventory().addIngredient(plant1 , 4); //Add 4 Plants to the inventory
         testPlayer.getInventory().setGold(7);
         
-        GameController.getInstance().getRoundOneController().TransmuteIngredient(testPlayer, plant1.getType());
+        GameController.getInstance().getRoundOneController().TransmuteIngredient(testPlayer, plant1);
         
-        assertEquals(3, testPlayer.getInventory().getIngredients().get(plant1));
+        assertEquals(3, testPlayer.getInventory().getIngredientCount(plant1));
         assertEquals(8, testPlayer.getInventory().getGold());
     }
     
