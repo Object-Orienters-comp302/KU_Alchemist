64fda20d2e94800f0fa903d9297da79b4026bcd1
HasanGorkem <HORTA21@KU.EDU.TR>
2024-01-12 22:26:16 +0300

    Small color fix

--- src/UI/Components/Publish/BookPanel.java
+++ src/UI/Components/Publish/BookPanel.java
@@ -131,7 +131,7 @@ public class BookPanel extends JPanel {
         CircleButton.setCurrentPath(aspectPath);
         CircleButton.disable();
         if (endorserPanel==null) {
-            endorserPanel = new ColorChangingPanel("#c97222 ", "#c97222", 20, ColorChangingPanel.RoundingStyle.BOTH);
+            endorserPanel = new ColorChangingPanel("#C97222","#DE710D",  20, ColorChangingPanel.RoundingStyle.BOTH);
         }
         endorserPanel.setOpaque(false);
         endorserPanel.setBounds(confirmButton.getBounds());
