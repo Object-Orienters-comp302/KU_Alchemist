3292d6a1078384ba4216b4eb7989a90abe78908b
MeteYokus2402 <myokus21@ku.edu.tr>
2024-01-16 21:36:21 +0300

    Added Enums

--- src/Networking/GameAction.java
+++ src/Networking/GameAction.java
@@ -1,5 +1,6 @@
 package Networking;
 
+import javax.swing.*;
 import java.io.Serial;
 import java.io.Serializable;
 
@@ -7,16 +8,45 @@ public class GameAction implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L; // Ensures version compatibility, not sure how and why
     
-    private String actionType;
+    private ActionType actionType;
     private String details;
     
-    public GameAction(String actionType, String details) {
+    public GameAction(ActionType actionType, String details) {
         this.actionType = actionType;
         this.details = details;
     }
+    enum ActionType{
+        ADDED_PLAYER(""),
+        GOLD(""),
+        ARTIFACT(""),
+        POTION(""),
+        INGREDIENT(""),
+        DECK_ARTIFACT(""),
+        DECK_INGREDIENT(""),
+        SICKNESS(""),
+        REPUTATION(""),
+        FORAGERIGHT(""),
+        PAUSE(""),
+        START_LOGIN_SCREEN(""),
+        START_START_VIEW(""),
+        START_END_GAME_VIEW(""),
+        UPDATE_PLAYER(""),
+        UPDATE_DECK("");
+        
+        private final String string;
+        
+        ActionType(String string) {
+            this.string = string;
+        }
+        
+        @Override
+        public String toString() {
+            return string;
+        }
+    }
     
     // Getters and setters for actionType and details
-    public String getActionType() {
+    public ActionType getActionType() {
         return actionType;
     }
     
