3ae54d014c77f4dcdc51b24e394ef1605b1ffe57
Eray Altun <ealtun21@ku.edu.tr>
2024-01-18 01:20:24 +0300

    Made ActionType public

--- src/Networking/GameAction.java
+++ src/Networking/GameAction.java
@@ -24,7 +24,7 @@ public class GameAction implements Serializable {
         
     }
     
-    enum ActionType{
+    public enum ActionType{
         PLAYER_JOINED("PLAYER JOINED"),
         GOLD(""),
         ARTIFACT(""),
